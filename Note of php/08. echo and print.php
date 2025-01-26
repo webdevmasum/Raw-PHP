ae275946c1141111b8b77fde6c18938ae7ce479e
webdevmasum <masumislam0018@gmail.com>
2025-01-26 15:52:48 -0800

    echo and print

---
+++ Note of php/08. echo and print.php
@@ -0,0 +1,27 @@
+1. echo:
+    echo ekta statement, jeta kono value, variable, ba string directly output kore.
+    echo multiple parameters o accept korte pare (comma diye separate kore).
+    echo kono value return kore na, eta just output kore.
+
+Example:
+    echo "Hello, World!";
+    echo "This is ", "a test message";
+
+2. print:
+
+    print o output statement, kintu eta ekta function hisebe kaaj kore.
+    print ekta single parameter accept kore.
+    print always ekta integer value (1) return kore, jeita mainly error checking e use hote pare.
+    echo er cheye slow, karon print ekta function ar echo ekta language construct.
+
+Example:
+    print "Hello, World!";
+    print "This is a test message";
+
+Main Difference:
+    echo multiple parameters accept korte pare, kintu print only ekta parameter accept kore.
+    echo kono value return kore na, kintu print 1 return kore.
+
+
+    
+    
