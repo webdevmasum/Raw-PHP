c2a4486e181aedcd648a535ce7f6ece7e9a136ac
webdevmasum <masumislam0018@gmail.com>
2025-03-01 15:57:49 -0800

    php prime number another way

--- Note of php/13. prime number.php
+++ Note of php/13. prime number.php
@@ -32,7 +32,44 @@ Prime Number: Different Way to check prime number
 
 
 
+************************
+Prime Number: Another Way to check prime number
+************************
 
+    <?php
+    function printPrimeNumbers($start, $end) {
+        for ($num = $start; $num <= $end; $num++) {
+            if (gmp_prob_prime($num) > 0) {
+                echo $num . " ";
+            }
+        }
+    }
+
+    echo "Prime Numbers between 1 to 100:\n";
+    printPrimeNumbers(1, 100);
+?>
+
+
+Explain here:->
+
+<?php
+// printPrimeNumbers function ta prime numbers print korbe je start theke end porjonto
+    function printPrimeNumbers($start, $end) {
+        // num ke start theke end porjonto iterate kora hocche
+        for ($num = $start; $num <= $end; $num++) {
+            // gmp_prob_prime function diye check kora hocche je num ta prime kina
+            // jodi prime hoy tahole 1 er beshi return kore, sei khetre number ta print hobe
+            if (gmp_prob_prime($num) > 0) {
+                // prime number ta print korche
+                echo $num . " ";
+            }
+        }
+    }
+
+    // ekhon function ta call kora hocche, jekhane prime numbers 1 theke 100 er moddhe print korbe
+    echo "Prime Numbers between 1 to 100:\n"; 
+    printPrimeNumbers(1, 100);  // 1 theke 100 er moddhe prime numbers print korbe
+?>
 
 
 
