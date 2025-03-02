91cb78e0bc2590e70e9dc397e1cbe6cc9c72a6f6
webdevmasum <masumislam0018@gmail.com>
2025-03-01 16:08:33 -0800

    php for loop array return another way

--- Note of php/15. for loop array print.php
+++ Note of php/15. for loop array print.php
@@ -19,6 +19,25 @@ For Loop Array print
 For Loop Array print another way
 *********************
 
+<?php
+    // Function to print array elements using implode
+    function printArray($array) {
+        // Checking if the array is not empty
+        if (empty($array)) {
+            echo "The array is empty.";
+            return;
+        }
+
+        // Printing array elements as a space-separated string
+        echo implode(" ", $array);
+    }
+
+    // Example array
+    $numbers = [1, 2, 3, 4, 5];
+
+    // Function call to print the array
+    printArray($numbers);
+?>
 
 
 
