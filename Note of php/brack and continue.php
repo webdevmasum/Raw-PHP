d9d9b07c24ed21c756ee90689f856e86f45c1d8c
webdevmasum <masumislam0018@gmail.com>
2025-03-01 17:33:43 -0800

    php break

---
+++ Note of php/brack and continue.php
@@ -0,0 +1,38 @@
+********************
+break Example:
+********************
+
+<?php
+    for ($i = 1; $i <= 10; $i++) {
+        if ($i == 5) {
+            break;  // Loop ta 5 er por break kore dibe
+        }
+        echo "Number: $i\n";
+    }
+?>
+
+
+Output:
+    Number: 1
+    Number: 2
+    Number: 3
+    Number: 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
