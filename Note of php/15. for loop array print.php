b79fb687b556aa8896374c5fc5b2e7ca25f96efb
webdevmasum <masumislam0018@gmail.com>
2025-03-01 16:07:58 -0800

    php for loop array return

---
+++ Note of php/15. for loop array print.php
@@ -0,0 +1,29 @@
+*********************
+For Loop Array print 
+*********************
+
+
+<?php
+    // Example array
+    $numbers = [1, 2, 3, 4, 5];
+
+    // For loop diye array print kora
+    for ($i = 0; $i < count($numbers); $i++) {
+        echo $numbers[$i] . " "; // array er prottekta element print hobe
+    }
+?>
+
+
+
+*********************
+For Loop Array print another way
+*********************
+
+
+
+
+
+
+
+
+
