1ed834d62be647fa5c00ba563fcc2667d0e8786b
webdevmasum <masumislam0018@gmail.com>
2025-03-01 17:15:49 -0800

    php for each basic

---
+++ Note of php/16. foreach loop.php
@@ -0,0 +1,38 @@
+*******************
+Foreach Loop Example
+*******************
+
+
+<?php
+    $students = ["Rony", "Mia", "Sima", "Alok"];
+
+    foreach ($students as $student) {
+        echo $student . "<br>";
+    }
+?>
+
+Output:
+    Rony
+    Mia
+    Sima
+    Alok
+
+
+*******************
+Foreach Loop Example
+*******************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
