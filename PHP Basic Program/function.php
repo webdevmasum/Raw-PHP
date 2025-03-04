fd00a52d88d0efacc7a8a9a3af84e4221e7c4983
Md. Masum Miah <167614546+webdevmasum@users.noreply.github.com>
2025-03-04 23:15:23 +0600

    php basic function noted

---
+++ PHP Basic Program/function.php
@@ -0,0 +1,40 @@
+
+
+🔹 Basic Function likha
+
+    <?php
+        function sayHello() {
+            echo "Hello, World!";
+        }
+
+        // Function call
+        sayHello();
+    ?>
+
+    Output: Hello, World!
+
+
+
+🔹 Function with Parameters    
+
+
+    <?php
+        function greet($name) {
+            echo "Hello, $name!";
+        }
+
+        // Function call
+        greet("Rahim");
+    ?>
+
+Output: Hello, Rahim!
+
+
+
+
+
+
+
+
+
+
