0bade53fe94601989bb690b34f65628f5d39cd51
Md. Masum Miah <167614546+webdevmasum@users.noreply.github.com>
2025-03-04 23:20:11 +0600

    php advanced function noted

--- PHP Basic Program/function.php
+++ PHP Basic Program/function.php
@@ -30,7 +30,39 @@
 Output: Hello, Rahim!
 
 
+🔹 Function with Return Value
 
+
+    <?php
+        function add($a, $b) {
+            return $a + $b;
+        }
+
+        // Function call & output
+        $result = add(5, 10);
+        echo "Sum: " . $result;
+    ?>
+
+Output: Sum: 15
+
+
+
+
+🔹 Default Parameter Function
+
+
+    <?php
+        function welcome($name = "Guest") {
+            echo "Welcome, $name!";
+        }
+
+        // Function call
+        welcome(); // Default value use hobe
+        welcome("Karim");
+    ?>
+
+Output: Welcome, Guest
+Welcome, Karim
 
 
 
