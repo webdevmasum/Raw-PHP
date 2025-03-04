a21f26e0e4fd8aa07dc039eb9c6181d9a7123e75
Md. Masum Miah <167614546+webdevmasum@users.noreply.github.com>
2025-03-04 22:45:34 +0600

    variable swap without third variable

--- PHP_Problem_Solve/basic_problem_three.php
+++ PHP_Problem_Solve/basic_problem_three.php
@@ -11,3 +11,33 @@
         // Test
         echo checkNumber(-5); // Output: Negative
     ?>
+
+
+
+✅ Problem 2: Swap Two Variables Without Using a Third Variable
+
+    <?php
+        function swapNumbers(&$a, &$b) {
+            $a = $a + $b;
+            $b = $a - $b;
+            $a = $a - $b;
+        }
+
+        // Test
+        $a = 5; $b = 10;
+        swapNumbers($a, $b);
+        echo "a = $a, b = $b"; // Output: a = 10, b = 5
+    ?>
+
+
+
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
