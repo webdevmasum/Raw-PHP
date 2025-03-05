38397bb71071a186379422ab7b7a20cd8a12abbc
Md. Masum Miah <167614546+webdevmasum@users.noreply.github.com>
2025-03-05 21:06:56 +0600

    php celsius to Fahrenheit

--- PHP_Problem_Solve/basic_problem_solve.php
+++ PHP_Problem_Solve/basic_problem_solve.php
@@ -10,3 +10,37 @@
         // Test
         echo isPerfectSquare(25); // Output: Perfect Square
     ?>
+
+
+
+✅ Problem 2: Convert Celsius to Fahrenheit
+
+    <?php
+        function celsiusToFahrenheit($celsius) {
+            return ($celsius * 9/5) + 32;
+        }
+
+        // Test
+        echo celsiusToFahrenheit(0); // Output: 32
+    ?>
+
+
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
+✅
